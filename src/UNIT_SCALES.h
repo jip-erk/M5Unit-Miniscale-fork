ad8cb63dfc293a9b6184a3e2126e92d709f8a5d5
Jip Erkelens <59746858+jip-erk@users.noreply.github.com>
2024-11-14 09:42:42 +0100

    Update UNIT_SCALES.h

--- src/UNIT_SCALES.h
+++ src/UNIT_SCALES.h
@@ -17,6 +17,7 @@
 #define UNIT_SCALES_CAL_DATA_INT_REG    0x60
 #define UNIT_SCALES_CAL_DATA_STRING_REG 0x70
 #define UNIT_SCALES_FILTER_REG          0x80
+#define UNIT_SCALES_FULL_WEIGHT_REG     0x90
 #define JUMP_TO_BOOTLOADER_REG          0xFD
 #define FIRMWARE_VERSION_REG            0xFE
 #define I2C_ADDRESS_REG                 0xFF
@@ -57,6 +58,10 @@ class UNIT_SCALES {
     uint8_t getAvgFilter(void);
     bool setEmaFilter(uint8_t ema);
     uint8_t getEmaFilter(void);
+
+    void setFullWeight(void); 
+    void setFullWeightValue(float weight);  
+    float getFullWeight(void); 
 };
 
 #endif
